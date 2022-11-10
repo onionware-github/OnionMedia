7e1afbd3a9ad03cf31beabce2a43d514654b15ea
Jaden <91136545+onionware-github@users.noreply.github.com>
2022-11-10 22:06:56 +0100

    Added minimum window size
    Minimal window size: 330x520

--- OnionMedia/OnionMedia/App.xaml.cs
+++ OnionMedia/OnionMedia/App.xaml.cs
@@ -117,6 +117,7 @@ namespace OnionMedia
                 var ffmpegStartup = IoC.Default.GetService<IFFmpegStartup>();
                 await ffmpegStartup.InitializeFormatsAndCodecsAsync();
                 CenterMainWindow();
+                SubClassing();
                 await activationService.ActivateAsync(args);
 
                 //Enable Mica
@@ -256,6 +257,64 @@ namespace OnionMedia
                 case ElementTheme.Light: m_configurationSource.Theme = Microsoft.UI.Composition.SystemBackdrops.SystemBackdropTheme.Light; break;
                 case ElementTheme.Default: m_configurationSource.Theme = Microsoft.UI.Composition.SystemBackdrops.SystemBackdropTheme.Default; break;
             }
+        }
+
+        //Native resizing stuff
+        private delegate IntPtr WinProc(IntPtr hWnd, PInvoke.User32.WindowMessage Msg, IntPtr wParam, IntPtr lParam);
+        private WinProc newWndProc = null;
+        private IntPtr oldWndProc = IntPtr.Zero;
+        [DllImport("user32")]
+        private static extern IntPtr SetWindowLongPtr(IntPtr hWnd, PInvoke.User32.WindowLongIndexFlags nIndex, WinProc newProc);
+        [DllImport("user32.dll")]
+        static extern IntPtr CallWindowProc(IntPtr lpPrevWndFunc, IntPtr hWnd, PInvoke.User32.WindowMessage Msg, IntPtr wParam, IntPtr lParam);
+
+        private void SubClassing()
+        {
+            //Get the Window's HWND
+            var hwnd = MainWindow.As<IWindowNative>().WindowHandle;
+
+            newWndProc = new WinProc(NewWindowProc);
+            oldWndProc = SetWindowLongPtr(hwnd, PInvoke.User32.WindowLongIndexFlags.GWL_WNDPROC, newWndProc);
+        }
+
+        int MinWidth = 330;
+        int MinHeight = 520;
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct MINMAXINFO
+        {
+            public PInvoke.POINT ptReserved;
+            public PInvoke.POINT ptMaxSize;
+            public PInvoke.POINT ptMaxPosition;
+            public PInvoke.POINT ptMinTrackSize;
+            public PInvoke.POINT ptMaxTrackSize;
+        }
+
+        private IntPtr NewWindowProc(IntPtr hWnd, PInvoke.User32.WindowMessage Msg, IntPtr wParam, IntPtr lParam)
+        {
+            switch (Msg)
+            {
+                case PInvoke.User32.WindowMessage.WM_GETMINMAXINFO:
+                    var dpi = PInvoke.User32.GetDpiForWindow(hWnd);
+                    float scalingFactor = (float)dpi / 96;
+
+                    MINMAXINFO minMaxInfo = Marshal.PtrToStructure<MINMAXINFO>(lParam);
+                    minMaxInfo.ptMinTrackSize.x = (int)(MinWidth * scalingFactor);
+                    minMaxInfo.ptMinTrackSize.y = (int)(MinHeight * scalingFactor);
+                    Marshal.StructureToPtr(minMaxInfo, lParam, true);
+                    break;
+
+            }
+            return CallWindowProc(oldWndProc, hWnd, Msg, wParam, lParam);
+        }
+
+
+        [ComImport]
+        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        [Guid("EECDBF0E-BAE9-4CB6-A68E-9598E1CB57BB")]
+        internal interface IWindowNative
+        {
+            IntPtr WindowHandle { get; }
         }
     }
 }
